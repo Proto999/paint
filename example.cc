4bf32bfffe2f6017ed1e7980cffd343ade0e633f
Artem <124712365+Proto999@users.noreply.github.com>
2023-06-24 03:26:04 +0300

    Update example.cc

--- example.cc
+++ example.cc
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <vector>
 #include <cstdlib>
-#include <cairo/cairo.h>  // Включаем библиотеку Cairo
 
 #include "window.h"
 #include "button.h"
 #include "GUI.h"
+#include "cairo.h"
 
 enum UserEventType
 {
@@ -23,7 +23,8 @@ enum UserEventType
 	EVENT_GREEN,
 	EVENT_BLUE,
 	EVENT_BLACK,
-	EVENT_HELP
+	EVENT_HELP,
+	
 };
 
 std::vector<Point>	g_vector;
@@ -54,21 +55,6 @@ private:
 	RGB         m_color;
 };
 
-void drawLine(Context *cr, const Point &start, const Point &end)
-{
-	cr->MoveTo(start);
-	cr->LineTo(end);
-	cr->Stroke();
-}
-
-void drawRectangle(Context *cr, const Point &topLeft, const Point &bottomRight)
-{
-	double width = bottomRight.GetX() - topLeft.GetX();
-	double height = bottomRight.GetY() - topLeft.GetY();
-	cr->Rectangle(topLeft, width, height);
-	cr->Stroke();
-}
-
 void MainWindow::OnDraw(Context *cr)
 {
 	std::cout << "MainWindow::OnDraw()" << std::endl;
@@ -82,6 +68,14 @@ void MainWindow::OnDraw(Context *cr)
 	cr->SetColor(RGB(1,1,1));
 	cr->FillRectangle(Point(0,0), Point(1000, 600));
 
+	cr->SetColor(RGB(0, 0, 0));  // Цвет прямой (черный)
+	cr->SetLineWidth(2);         // Толщина линии
+	cr->Line(g_vector[0], g_vector[1]);
+
+	cr->SetColor(RGB(0, 0, 0));   // Цвет прямоугольника (черный)
+	cr->SetLineWidth(2);          // Толщина линии
+	cr->Rectangle(g_vector[0], g_vector[1]);
+
 	cr->SetColor(RGB(0,0,0));
 	cr->SetLineWidth(3);
 	cr->Line(Point(0,600), Point(1000, 600));
@@ -89,132 +83,320 @@ void MainWindow::OnDraw(Context *cr)
 		cr->Text(" Moving ", "Cantarel", 17, Point(900, 625), 0x01);
 	else if (g_mode == 1)
 		cr->Text(" Printing ", "Cantarel", 17, Point(900, 625), 0x01);
-		else
+	else if (g_mode == 2)
 		cr->Text(" Erasing ", "Cantarel", 17, Point(900, 625), 0x01);
+	else if (g_mode == 3)
+		cr->Text(" Line ", "Cantarel", 17, Point(900, 625), 0x01);
+	else if (g_mode == 4)
+		cr->Text(" Rectangle ", "Cantarel", 17, Point(900, 625), 0x01);
+	sprintf(g_text, "%ld", g_vector.size());
+	cr->Text(g_text, "Cantarel", 17, Point(900, 650), 0x01);
 
-	// Рисуем прямые
-	if (g_mode == 0 || g_mode == 1) {
-		for (size_t i = 0; i < g_vector.size() - 1; i += 2) {
-			cr->SetLineWidth(1);
-			cr->SetColor(RGB(g_color_v[i], g_color_v[i + 1], g_color_v[i + 2]));
-			drawLine(cr, g_vector[i], g_vector[i + 1]);
-		}
+	for (int i = 0; i < g_vector.size(); i++)
+	{
+		if (g_color_v[i] == 0)
+			cr->SetColor(RGB(0,0,0));
+		if (g_color_v[i] == 1)
+			cr->SetColor(RGB(1, 0, 0));
+		if (g_color_v[i] == 2)
+			cr->SetColor(RGB(0, 1, 0));
+		if (g_color_v[i] == 3)
+			cr->SetColor(RGB(0, 0, 1));
+		cr->FillRectangle(g_vector[i], g_psize);
 	}
 
-	// Рисуем прямоугольники
-	if (g_mode == 0 || g_mode == 2) {
-		cr->SetLineWidth(1);
-		cr->SetColor(RGB(0, 0, 0));
-		for (size_t i = 0; i < g_vector.size() - 1; i += 2) {
-			Point topLeft = g_vector[i];
-			Point bottomRight = g_vector[i + 1];
-			drawRectangle(cr, topLeft, bottomRight);
-		}
+	if (g_help == 1)
+	{
+		cr->SetColor(RGB(0.8,0.8,0.8));
+		cr->FillRectangle(Point(0,0), Point(1000, 600));
+		cr->SetColor(RGB(0,0,0));
+		cr->Text("УПРАВЛЕНИЕ:", "Cantarel", 20, Point(500, 50), 0x01);
+		cr->Text("W-A-S-D / СТРЕЛКИ / МЫШЬ - ПЕРЕМЕЩЕНИЕ КУРСОРА", "Cantarel", 20, Point(500, 100), 0x01);
+		cr->Text("E - СМЕНА РЕЖИМА УПРАВЛЕНИЯ (РИСОВАНИЕ, СТИРАНИЕ, ПЕРЕМЕЩЕНИЕ)", "Cantarel", 20, Point(500, 150), 0x01);
+		cr->Text("ЛКМ - ПОСТАВИТЬ ТОЧКУ", "Cantarel", 20, Point(500, 200), 0x01);
+		cr->Text("ПКМ - УБРАТЬ ТОЧКУ", "Cantarel", 20, Point(500, 250), 0x01);
+		cr->Text("R - ВЫБРАТЬ КРАСНЫЙ ЦВЕТ", "Cantarel", 20, Point(500, 300), 0x01);
+		cr->Text("G - ВЫБРАТЬ ЗЕЛЕНЫЙ ЦВЕТ", "Cantarel", 20, Point(500, 350), 0x01);
+		cr->Text("B - ВЫБРАТЬ СИНИЙ ЦВЕТ", "Cantarel", 20, Point(500, 400), 0x01);
+		cr->Text("N - ВЫБРАТЬ ЧЕРНЫЙ ЦВЕТ", "Cantarel", 20, Point(500, 450), 0x01);
+		cr->Text("С - ОЧИСТИТЬ ХОЛСТ", "Cantarel", 20, Point(500, 500), 0x01);
+		cr->Text("Q - ВЫХОД", "Cantarel", 20, Point(500, 550), 0x01);
+	}
+
+	cr->SetColor(RGB(0.5,0.1,0.9));
+	cr->FillRectangle(g_coords, g_psize);
+}
+
+void MainWindow::OnCreate()
+{
+	std::cout << "MainWindow::OnCreate()" << std::endl;
+
+	AddChild(new Button(" UP ",EVENT_UP), Point(450,600), Point(100,50));
+	AddChild(new Button(" DOWN ",EVENT_DOWN), Point(450,650), Point(100,50));
+	AddChild(new Button(" <- ",EVENT_LEFT), Point(350,650), Point(100,50));
+
+
+	AddChild(new Button(" -> ",EVENT_RIGHT), Point(550,650), Point(100,50));
+	
+
+	AddChild(new Button(" SET ",EVENT_SET), Point(150,650), Point(50,50));
+	AddChild(new Button("UNSET",EVENT_UNSET), Point(200,650), Point(50,50));
+	AddChild(new Button(" CLEAR ",EVENT_CLEAR), Point(150,600), Point(100,50));
+	AddChild(new Button(" MODE ",EVENT_CHMODE), Point(750,600), Point(100,50));
+	AddChild(new Button(" R ",EVENT_RED), Point(750,650), Point(25,50));
+	AddChild(new Button(" G ",EVENT_GREEN), Point(775,650), Point(25,50));
+	AddChild(new Button(" B ",EVENT_BLUE), Point(800,650), Point(25,50));
+	AddChild(new Button(" BL ",EVENT_BLACK), Point(825,650), Point(25,50));
+	AddChild(new Button(" HELP ",EVENT_HELP), Point(50,600), Point(100,50));
+}
+
+void	SetPoint(void)
+{
+	g_color_v.push_back(g_color_f);
+	// if (g_color_f == 0)
+	// 	g_color_v.push_back(0);
+	// else if (g_color_f == 1)
+	// 	g_color_v.push_back(1);
+	// else if (g_color_f == 2)
+	// 	g_color_v.push_back(2);
+	// else if (g_color_f == 3)
+	// 	g_color_v.push_back(3);
+	g_vector.push_back(g_coords);
+}
+
+void	UnsetPoint(void)
+{
+	for (int i = 0; i < g_vector.size(); i++)
+	{
+		if (abs(g_vector[i].GetX() - g_coords.GetX()) < 10 && abs(g_vector[i].GetY() - g_coords.GetY()) < 10)
+		{
+			g_color_v.erase(g_color_v.cbegin() + i);
+			g_vector.erase(g_vector.cbegin() + i);
+		}	 
 	}
 }
 
+void MainWindow::OnNotify(Window *child, uint32_t type, const Point &position)
+{
+	std::cout << "MainWindow::OnNotify()" << std::endl;
+	if(type == EVENT_CLOSE)
+	{
+		DeleteMe();
+	}
+	else if(type == EVENT_UP && g_coords.GetY() - g_psize.GetY() >= 0)
+	{
+		if (g_mode == 1)
+			SetPoint();
+		if (g_mode == 2)
+			UnsetPoint();
+		g_coords.SetY(g_coords.GetY() - g_psize.GetY());
+		ReDraw();
+	}
+	else if(type == EVENT_DOWN && g_coords.GetY() + g_psize.GetY() <= (600 - g_psize.GetY()))
+	{
+		if (g_mode == 1)
+			SetPoint();
+		if (g_mode == 2)
+			UnsetPoint();
+		g_coords.SetY(g_coords.GetY() + g_psize.GetY());
+		ReDraw();
+	}
+	else if(type == EVENT_LEFT && g_coords.GetX() - g_psize.GetX() >= 0)
+	{
+		if (g_mode == 1)
+			SetPoint();
+		if (g_mode == 2)
+			UnsetPoint();
+		g_coords.SetX(g_coords.GetX() - g_psize.GetX());
+		ReDraw();
+	}
+	else if(type == EVENT_RIGHT && g_coords.GetX() + g_psize.GetX() <= (1000 - g_psize.GetX()))
+	{
+		if (g_mode == 1)
+			SetPoint();
+		if (g_mode == 2)
+			UnsetPoint();
+		g_coords.SetX(g_coords.GetX() + g_psize.GetX());
+		ReDraw();
+	}
+	else if(type == EVENT_SET)
+	{
+		SetPoint();
+		ReDraw();
+	}
+	else if(type == EVENT_UNSET)
+	{
+		UnsetPoint();
+		ReDraw();
+	}
+	else if(type == EVENT_CLEAR)
+	{
+		g_vector.clear();
+		ReDraw();
+	}
+	else if(type == EVENT_CHMODE)
+	{
+		g_mode++;
+		if (g_mode == 2)
+			g_mode = 0;
+		ReDraw();
+	}
+	else if(type == EVENT_RED)
+	{
+		g_color_f = 1;
+		ReDraw();
+	}
+	else if(type == EVENT_GREEN)
+	{
+		g_color_f = 2;
+		ReDraw();
+	}
+	else if(type == EVENT_BLUE)
+	{
+		g_color_f = 3;
+		ReDraw();
+	}
+	else if(type == EVENT_BLACK)
+	{
+		g_color_f = 0;
+		ReDraw();
+	}
+	else if(type == EVENT_HELP)
+	{
+		g_help = !g_help;
+		ReDraw();
+	}	
+	else if(type == EVENT_CHMODE g_coords.GetX() + g_psize.GetX())
+	{
+		g_mode++;
+		if (g_mode == 3) //LINE
+			DrawLine(cr, g_vector[0], g_vector[1]);	
+		ReDraw();
+	}
+}	
+
 bool MainWindow::OnMouseMove(const Point &position)
 {
-	g_coords = position;
-	NotifyChildren(EVENT_UP, g_coords);
-	NotifyChildren(EVENT_DOWN, g_coords);
-	NotifyChildren(EVENT_LEFT, g_coords);
-	NotifyChildren(EVENT_RIGHT, g_coords);
-
+	// std::cout << "MainWindow::OnMouseMove(" << position.GetX() << "," << position.GetY() << ")" << std::endl;
+	if (g_mode == 1)
+		SetPoint();
+	if (g_mode == 2)
+		UnsetPoint();
+	if (position.GetX() <= (1000 - g_psize.GetX()) && (position.GetY() <= (600 - g_psize.GetY())))
+	{
+		g_coords.SetX(position.GetX());
+		g_coords.SetY(position.GetY());
+	}
+	ReDraw();
 	return true;
 }
 
 bool MainWindow::OnLeftMouseButtonClick(const Point &position)
 {
-	g_coords = position;
-	NotifyChildren(EVENT_SET, g_coords);
+	SetPoint();
+	ReDraw();
 	return true;
 }
 
 bool MainWindow::OnRightMouseButtonClick(const Point &position)
 {
-	g_coords = position;
-	NotifyChildren(EVENT_UNSET, g_coords);
+    UnsetPoint();
+	ReDraw();
 	return true;
 }
 
-void MainWindow::OnNotify(Window *child, uint32_t type, const Point &position)
+bool MainWindow::OnKeyPress(uint64_t keyval)
 {
-	if (type == EVENT_CLOSE)
-		Close();
-	else if (type == EVENT_RED)
-		m_color = RGB(1, 0, 0);
-	else if (type == EVENT_GREEN)
-		m_color = RGB(0, 1, 0);
-	else if (type == EVENT_BLUE)
-		m_color = RGB(0, 0, 1);
-	else if (type == EVENT_BLACK)
-		m_color = RGB(0, 0, 0);
-	else if (type == EVENT_CHMODE) {
+	std::cout << "MainWindow::OnKeyPress(" << keyval << ")" << std::endl;
+
+	if (keyval == 'w' || keyval == 65362)
+	{
+		if(g_coords.GetY() - g_psize.GetY() >= 0)
+		{
+			if (g_mode == 1)
+				SetPoint();
+			if (g_mode == 2)
+				UnsetPoint();
+			g_coords.SetY(g_coords.GetY() - g_psize.GetY());
+		}
+	}
+	if (keyval == 'a' || keyval == 65361)
+	{
+		if(g_coords.GetX() - g_psize.GetX() >= 0)
+		{
+			if (g_mode == 1)
+				SetPoint();
+			if (g_mode == 2)
+				UnsetPoint();
+			g_coords.SetX(g_coords.GetX() - g_psize.GetX());
+		}
+	}
+	if (keyval == 's' || keyval == 65364)
+	{
+		if(g_coords.GetY() + g_psize.GetY() <= (600 - g_psize.GetY()))
+		{
+			if (g_mode == 1)
+				SetPoint();
+			if (g_mode == 2)
+				UnsetPoint();
+			g_coords.SetY(g_coords.GetY() + g_psize.GetY());
+		}
+	}
+	if (keyval == 'd' || keyval == 65363)
+	{
+		if(g_coords.GetX() + g_psize.GetX() <= (1000 - g_psize.GetX()))
+		{
+			if (g_mode == 1)
+				SetPoint();
+			if (g_mode == 2)
+				UnsetPoint();
+			g_coords.SetX(g_coords.GetX() + g_psize.GetX());
+		}
+	}
+	if (keyval == 'c')
+	{
+		g_vector.clear();
+	}
+	if (keyval == 'q')
+	{
+		DeleteMe();
+		return true;
+	}
+	if (keyval == 'e')
+	{
 		g_mode++;
-		if (g_mode > 2)
+		if (g_mode == 3)
 			g_mode = 0;
 	}
-	else if (type == EVENT_UP)
+	if (keyval == 'n')
 	{
-		if (g_help == 1)
-			ShowHelp(0);
-		g_help = 0;
-		g_psize.SetY(g_psize.GetY() + 1);
+		g_color_f = 0;
 	}
-	else if (type == EVENT_DOWN)
+	if (keyval == 'r')
 	{
-		if (g_help == 1)
-			ShowHelp(0);
-		g_help = 0;
-		g_psize.SetY(g_psize.GetY() - 1);
-		if (g_psize.GetY() < 1)
-			g_psize.SetY(1);
+		g_color_f = 1;
 	}
-	else if (type == EVENT_LEFT)
+	if (keyval == 'g')
 	{
-		if (g_help == 1)
-			ShowHelp(0);
-		g_help = 0;
-		g_psize.SetX(g_psize.GetX() - 1);
-		if (g_psize.GetX() < 1)
-			g_psize.SetX(1);
+		g_color_f = 2;
 	}
-	else if (type == EVENT_RIGHT)
+	if (keyval == 'b')
 	{
-		if (g_help == 1)
-			ShowHelp(0);
-		g_help = 0;
-		g_psize.SetX(g_psize.GetX() + 1);
+		g_color_f = 3;
 	}
-	else if (type == EVENT_SET)
-	{
-		if (g_help == 1)
-			ShowHelp(0);
-		g_help = 0;
-		if (g_vector.size() % 2 == 0)
-		{
-			g_vector.push_back(position);
-			g_color_v.push_back(m_color.GetR());
-			g_color_v.push_back(m_color.GetG());
-			g_color_v.push_back(m_color.GetB());
-		}
-		else
-		{
-			g_vector[g_vector.size() - 1] = position;
-		}
-	}
-	else if (type == EVENT_UNSET)
-	{
-		if (g_help == 1)
-			ShowHelp(0);
-		g_help = 0;
-		size_t closestIndex = FindClosestPointIndex(g_vector, position);
-		if (closestIndex != -1)
-		{
-			g_vector.erase(g_vector.begin() + closestIndex, g_vector.begin() + closestIndex + 2);
-			g_color_v.erase(g_color_v.begin() + closestIndex * 3, g_color_v.begin() + closestIndex * 3 + 3);
-		}
-	}
+	ReDraw();
+	return true;
 }
 
+
+// функция main
+int main(int argc, char **argv)
+{
+	MainWindow *pWindow = new MainWindow;
+
+	int res = Run(argc, argv, pWindow, 1000, 700);
+
+	delete pWindow;
+
+	return res;
+}
